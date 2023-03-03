d5d8eb6470898d269984fa82f49a01d53f375eff
ImamSudiyatmo <sudiyatmoimam@gmail.com>
2023-03-03 12:45:29 +0700

    add view link to admin

---
+++ app/Views/admin/wrapper.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="<?= env('App.defaultLocale') ?>">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= isset($title) ? $title : '' ?></title>
+  <?= $this->renderSection('css') ?>
+</head>
+
+<body>
+  <?= $this->renderSection('content') ?>
+  <?= $this->renderSection('js') ?>
+</body>
+
+</html>
