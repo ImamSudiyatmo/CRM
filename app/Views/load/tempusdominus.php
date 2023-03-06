635d3dc0d7d926e4aa4eff5bc1c837c73d7151a7
ImamSudiyatmo <sudiyatmoimam@gmail.com>
2023-03-06 15:48:06 +0700

    edit tempusdominus load icon view

--- app/Views/load/tempusdominus.php
+++ app/Views/load/tempusdominus.php
@@ -1,10 +1,25 @@
 <?= $this->section('css') ?>
-<link rel="stylesheet" href="<?= base_url('assets/tempusdominus/build/css/tempusdominus-bootstrap-4.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/tempus-dominus/build/css/tempusdominus-bootstrap-4.min.css') ?>">
 <?= $this->endSection() ?>
 
 <?= $this->include('load/jquery') ?>
 <?= $this->include('load/moment') ?>
 
 <?= $this->section('js') ?>
-<script src="<?= base_url('assets/tempusdominus/build/js/tempusdominus-bootstrap-4.min.js') ?>"></script>
+<script src="<?= base_url('assets/tempus-dominus/build/js/tempusdominus-bootstrap-4.min.js') ?>"></script>
+<script>
+  $.fn.datetimepicker.Constructor.Default = $.extend({}, $.fn.datetimepicker.Constructor.Default, {
+    icons: {
+      time: 'bi bi-clock',
+      date: 'bi bi-calendar2-fill',
+      up: 'bi bi-arrow-up',
+      down: 'bi bi-arrow-down',
+      previous: 'bi bi-chevron-left',
+      next: 'bi bi-chevron-right',
+      today: 'bi bi-calendar-check',
+      clear: 'bi bi-trash',
+      close: 'bi bi-times'
+    }
+  });
+</script>
 <?= $this->endSection() ?>
