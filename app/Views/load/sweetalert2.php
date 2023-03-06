f1c9dd8c9ae140d713e357e0de5b6ab5d4a7f19b
ImamSudiyatmo <sudiyatmoimam@gmail.com>
2023-03-06 15:59:30 +0700

    add sweetalert2 load view

---
+++ app/Views/load/sweetalert2.php
@@ -0,0 +1,7 @@
+<?= $this->section('css') ?>
+<link rel="stylesheet" href="<?= base_url('assets/sweetalert2/dist/sweetalert2.min.css') ?>">
+<?= $this->endSection() ?>
+
+<?= $this->section('js') ?>
+<script src="<?= base_url('assets/sweetalert2/dist/sweetalert2.min.js') ?>"></script>
+<?= $this->endSection() ?>
