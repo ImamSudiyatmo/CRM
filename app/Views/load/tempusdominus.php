2d849fb7bd6ba171c44b81664f44c71f81286a0b
ImamSudiyatmo <sudiyatmoimam@gmail.com>
2023-03-06 15:22:08 +0700

    add tempusdominus load view

---
+++ app/Views/load/tempusdominus.php
@@ -0,0 +1,10 @@
+<?= $this->section('css') ?>
+<link rel="stylesheet" href="<?= base_url('assets/tempusdominus/build/css/tempusdominus-bootstrap-4.min.css') ?>">
+<?= $this->endSection() ?>
+
+<?= $this->include('load/jquery') ?>
+<?= $this->include('load/moment') ?>
+
+<?= $this->section('js') ?>
+<script src="<?= base_url('assets/tempusdominus/build/js/tempusdominus-bootstrap-4.min.js') ?>"></script>
+<?= $this->endSection() ?>
