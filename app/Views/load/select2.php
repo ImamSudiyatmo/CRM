4d9301ab0074a1b240d2523f07d1ebc360cd6769
ImamSudiyatmo <sudiyatmoimam@gmail.com>
2023-03-06 16:19:25 +0700

    add load view select2

---
+++ app/Views/load/select2.php
@@ -0,0 +1,7 @@
+<?= $this->section('css') ?>
+<link rel="stylesheet" href="<?= base_url('assets/select2/dist/css/select2.min.css') ?>">
+<?= $this->endSection() ?>
+
+<?= $this->section('js') ?>
+<script src="<?= base_url('assets/select2/dist/js/select2.min.js') ?>"></script>
+<?= $this->endSection() ?>
