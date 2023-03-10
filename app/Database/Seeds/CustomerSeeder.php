dcef0df7500418474e39d143defb8ef18b0cd228
ImamSudiyatmo <sudiyatmoimam@gmail.com>
2023-03-10 15:45:53 +0700

    make customer seeder for dumb data

---
+++ app/Database/Seeds/CustomerSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\Test\Fabricator;
+use App\Models\CustomerModel;
+
+class CustomerSeeder extends Seeder
+{
+    public function run()
+    {
+        $generator = new Fabricator(CustomerModel::class, null, 'id_ID');
+        $generator->create(50);
+    }
+}
