f5af433cf11be8ddb038a54c5d61ed1c560eda36
ImamSudiyatmo <sudiyatmoimam@gmail.com>
2023-03-14 09:55:20 +0700

    add nav menu

--- app/Views/admin/wrapper.php
+++ app/Views/admin/wrapper.php
@@ -3,5 +3,11 @@
 <?= $this->extend('wrapper') ?>
 
 <?= $this->section('content') ?>
+<!-- Menu -->
+<nav class="nav nav-tabs">
+  <a href="<?= base_url('admin') ?>" class="nav-link <?= current_url() == base_url('admin') ? 'active' : '' ?>">Home</a>
+  <a href="<?= base_url('admin/customer') ?>" class="nav-link <?= current_url() == base_url('admin/customer') ? 'active' : '' ?>">Customer</a>
+</nav>
+<!-- End Menu -->
 <?= $this->renderSection('adminContent') ?>
 <?= $this->endSection() ?>
